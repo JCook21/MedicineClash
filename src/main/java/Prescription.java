cc13c843c81defe52dd31b9665d5b72af4b89303
mukhan <mushahidnawaz@hotmail.com>
2020-06-11 14:46:41 -0400

    Added getters and setters to the Prescription class.

--- src/main/java/Prescription.java
+++ src/main/java/Prescription.java
@@ -6,6 +6,22 @@ public class Prescription {
     private LocalDate dispenseDate;
     private int daysSupply = 30;
 
+    public LocalDate getDispenseDate() {
+        return dispenseDate;
+    }
+
+    public void setDispenseDate(LocalDate dispenseDate) {
+        this.dispenseDate = dispenseDate;
+    }
+
+    public int getDaysSupply() {
+        return daysSupply;
+    }
+
+    public void setDaysSupply(int daysSupply) {
+        this.daysSupply = daysSupply;
+    }
+
     public Prescription(LocalDate dispenseDate, int daysSupply) {
         this.dispenseDate = dispenseDate;
         this.daysSupply = daysSupply;
